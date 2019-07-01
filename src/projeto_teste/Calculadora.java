2f3771d0ff90bf6a160890a8c834f411a7d1d08e
AlanErick <ae.eremitad@gmail.com>
2019-07-01 01:08:29 -0300

    Vs. 2.0

--- src/projeto_teste/Calculadora.java
+++ src/projeto_teste/Calculadora.java
@@ -6,64 +6,16 @@ import javax.swing.JOptionPane;
 
 public class Calculadora implements Observer {
 
-    /*public void Soma() {
-        System.out.println("Soma");
-    }
-
-    public void sub() {
-        System.out.println("Subitração");
-    }
-
-    public void div() {
-        System.out.println("Divisão");
-    }
-
-    public void mult() {
-        System.out.println("Multiplicação");
-    }*/
     Observable formulas;
-    
+
     double novaOperacao;
-    
+
     public Calculadora(Observable formulas) {
         this.formulas = formulas;
         formulas.addObserver(this);
     }
-    
-    @Override
-    public void update(Observable formulasInfSubject, Object arg1) {
-        if (formulasInfSubject instanceof Formulas) {
-            Formulas formulas = (Formulas) formulasInfSubject;
-            novaOperacao = formulas.soma(novaOperacao, novaOperacao);
-            System.out.println("Informe: " + novaOperacao);
-        }
-    }   
-    
-    
-   /* @Override
-    public void update(Observable arg0, Object arg1) {
-        Formulas formulasObservadas = (Formulas) arg0;
-        String acao = String.valueOf(args1);
 
-        if (acao.equals("Somar")) {
-            this.Soma();
-        }
-
-        if (acao.equals("Subtrair")) {
-            this.sub();
-        }
-
-        if (acao.equals("Divisão")) {
-            this.div();
-        }
-
-        if (acao.equals("Multiplicação")) {
-            this.mult();
-        }
-
-    }*/
-
-    private Calculadora() {
+    Calculadora() {
     }
     //aplicar o Singleton
     private static Calculadora instancia;
@@ -75,7 +27,7 @@ public class Calculadora implements Observer {
         return instancia;
     }
 
-    public static void main(String[] args) {
+    public static void criador() {
         //declarando as variaveis
         double primeiro, segundo;
 
@@ -86,5 +38,14 @@ public class Calculadora implements Observer {
         Formulas resul = new Formulas();
 
         Formulas.Menu_operacao(resul, primeiro, segundo); //invocando o mentodo Extract Method                
+    }
+
+    @Override
+    public void update(Observable formulasInfSubject, Object arg1) {
+        if (formulasInfSubject instanceof Formulas) {
+            Formulas formulas = (Formulas) formulasInfSubject;
+            novaOperacao = formulas.soma(novaOperacao, novaOperacao);
+            System.out.println("Informe: " + novaOperacao);
+        }
     }
 }
